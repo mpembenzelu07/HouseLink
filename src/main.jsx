a6403e33172c2d49f403233fb6b40beb14cf5a50
mpembenzelu07 <mpembenzelu@gmail.com>
2025-12-04 13:52:16 +0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import AppRouter from './AppRouter';
-import './index.css';
-
-createRoot(document.getElementById('root')).render(<AppRouter />);
